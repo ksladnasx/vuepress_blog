0887630ce8d424492719e96d5b8759c5ad77a9fd
ksldnasx <wh8261408@126.com>
2026-02-06 17:38:07 +0800

    deploy

---
+++ assets/Nuxt4.html-De03BzNK.js
@@ -0,0 +1,45 @@
+import{_ as s,c as a,a as e,o as l}from"./app-UmXydJji.js";const i={};function p(c,n){return l(),a("div",null,[...n[0]||(n[0]=[e(`<h1 id="nuxt-minimal-starter" tabindex="-1"><a class="header-anchor" href="#nuxt-minimal-starter"><span>Nuxt Minimal Starter</span></a></h1><p>Look at the <a href="https://nuxt.com/docs/getting-started/introduction" target="_blank" rel="noopener noreferrer">Nuxt documentation</a> to learn more.</p><h2 id="setup" tabindex="-1"><a class="header-anchor" href="#setup"><span>Setup</span></a></h2><p>Make sure to install dependencies:</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code><span class="line"><span class="token comment"># npm</span></span>
+<span class="line"><span class="token function">npm</span> <span class="token function">install</span></span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># pnpm</span></span>
+<span class="line"><span class="token function">pnpm</span> <span class="token function">install</span></span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># yarn</span></span>
+<span class="line"><span class="token function">yarn</span> <span class="token function">install</span></span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># bun</span></span>
+<span class="line">bun <span class="token function">install</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="development-server" tabindex="-1"><a class="header-anchor" href="#development-server"><span>Development Server</span></a></h2><p>Start the development server on <code>http://localhost:3000</code>:</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code><span class="line"><span class="token comment"># npm</span></span>
+<span class="line"><span class="token function">npm</span> run dev</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># pnpm</span></span>
+<span class="line"><span class="token function">pnpm</span> dev</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># yarn</span></span>
+<span class="line"><span class="token function">yarn</span> dev</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># bun</span></span>
+<span class="line">bun run dev</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="production" tabindex="-1"><a class="header-anchor" href="#production"><span>Production</span></a></h2><p>Build the application for production:</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code><span class="line"><span class="token comment"># npm</span></span>
+<span class="line"><span class="token function">npm</span> run build</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># pnpm</span></span>
+<span class="line"><span class="token function">pnpm</span> build</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># yarn</span></span>
+<span class="line"><span class="token function">yarn</span> build</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># bun</span></span>
+<span class="line">bun run build</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Locally preview production build:</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code><span class="line"><span class="token comment"># npm</span></span>
+<span class="line"><span class="token function">npm</span> run preview</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># pnpm</span></span>
+<span class="line"><span class="token function">pnpm</span> preview</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># yarn</span></span>
+<span class="line"><span class="token function">yarn</span> preview</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># bun</span></span>
+<span class="line">bun run preview</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Check out the <a href="https://nuxt.com/docs/getting-started/deployment" target="_blank" rel="noopener noreferrer">deployment documentation</a> for more information.</p>`,14)])])}const d=s(i,[["render",p]]),r=JSON.parse('{"path":"/posts/nuxtlearning/Nuxt4.html","title":"Nuxt Minimal Starter","lang":"zh-CN","frontmatter":{"date":"2025-08-26T00:00:00.000Z","category":["杂项"],"tag":["Nuxt","SSR"]},"headers":[{"level":2,"title":"Setup","slug":"setup","link":"#setup","children":[]},{"level":2,"title":"Development Server","slug":"development-server","link":"#development-server","children":[]},{"level":2,"title":"Production","slug":"production","link":"#production","children":[]}],"git":{"updatedTime":1765244359000,"contributors":[{"name":"ksldnasx","username":"ksldnasx","email":"wh8261408@126.com","commits":2,"url":"https://github.com/ksldnasx"}],"changelog":[{"hash":"a0089923dda5b0a1e568fce8d29533128e78ce23","time":1765244359000,"email":"wh8261408@126.com","author":"ksldnasx","message":"fix:各类文件布局"},{"hash":"777080f9cb61625063573bffd7eb8f506620dc39","time":1762400469000,"email":"wh8261408@126.com","author":"ksldnasx","message":"init"}]},"filePathRelative":"posts/nuxtlearning/Nuxt4.md","excerpt":"\\n<p>Look at the <a href=\\"https://nuxt.com/docs/getting-started/introduction\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">Nuxt documentation</a> to learn more.</p>\\n<h2>Setup</h2>\\n<p>Make sure to install dependencies:</p>\\n<div class=\\"language-bash line-numbers-mode\\" data-highlighter=\\"prismjs\\" data-ext=\\"sh\\"><pre><code><span class=\\"line\\"><span class=\\"token comment\\"># npm</span></span>\\n<span class=\\"line\\"><span class=\\"token function\\">npm</span> <span class=\\"token function\\">install</span></span>\\n<span class=\\"line\\"></span>\\n<span class=\\"line\\"><span class=\\"token comment\\"># pnpm</span></span>\\n<span class=\\"line\\"><span class=\\"token function\\">pnpm</span> <span class=\\"token function\\">install</span></span>\\n<span class=\\"line\\"></span>\\n<span class=\\"line\\"><span class=\\"token comment\\"># yarn</span></span>\\n<span class=\\"line\\"><span class=\\"token function\\">yarn</span> <span class=\\"token function\\">install</span></span>\\n<span class=\\"line\\"></span>\\n<span class=\\"line\\"><span class=\\"token comment\\"># bun</span></span>\\n<span class=\\"line\\">bun <span class=\\"token function\\">install</span></span>\\n<span class=\\"line\\"></span></code></pre>\\n<div class=\\"line-numbers\\" aria-hidden=\\"true\\" style=\\"counter-reset:line-number 0\\"><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div><div class=\\"line-number\\"></div></div></div>"}');export{d as comp,r as data};
