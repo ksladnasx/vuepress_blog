b0b0fc8c9a1a4ccee45dfcd1bf76710d8dac8e6c
ksldnasx <wh8261408@126.com>
2026-05-25 22:55:27 +0800

    feat

--- tests/算法.js
+++ tests/算法.js
@@ -428,31 +428,382 @@ var reverseList = function (head) {
   return prev;
 };
 
-
 //岛屿数量
-var numIslands = function(grid) {
-  let r = grid.length
-  let c = grid[0].length
-  let count = 0
-  for(let i = 0;i<=r;i++){
-    for(let j = 0;j<= c;j++){
-      if (grid[i][j]){
-       if(i == 0 &j == 0){
-          count++
-       }else if(i == r-1 & j == 0){
-          if(grid[i-1][j] != 1 ){
-            count ++
+var numIslands = function (grid) {
+  let r = grid.length;
+  let c = grid[0].length;
+  let count = 0;
+  for (let i = 0; i <= r; i++) {
+    for (let j = 0; j <= c; j++) {
+      if (grid[i][j]) {
+        if ((i == 0) & (j == 0)) {
+          count++;
+        } else if ((i == r - 1) & (j == 0)) {
+          if (grid[i - 1][j] != 1) {
+            count++;
           }
-       }else if(i == 0 &j == c-1){
-          if(grid[i][j-1] != 1 ){
-            count ++
+        } else if ((i == 0) & (j == c - 1)) {
+          if (grid[i][j - 1] != 1) {
+            count++;
           }
-       }else if(grid[i-1][j] != 1 && grid[i][j-1] != 1){
-          count ++
-       }
-
+        } else if (grid[i - 1][j] != 1 && grid[i][j - 1] != 1) {
+          count++;
+        }
       }
-      
     }
   }
+};
+
+//动态规划
+var rob = function (nums) {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+  let val = new Array(nums.length).fill(0);
+  val[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (i == 1) {
+      val[i] = Math.max(nums[0], nums[1]);
+      continue;
+    }
+    val[i] = Math.max(val[i - 1], val[i - 2] + nums[i]);
+  }
+  return val[nums.length - 1];
+};
+
+console.log(rob([1, 2]));
+
+//字典计数
+var majorityElement = function (nums) {
+  let dict = {};
+
+  // 第一步：计数
+  for (let i = 0; i < nums.length; i++) {
+    if (dict[nums[i]]) {
+      dict[nums[i]]++;
+    } else {
+      dict[nums[i]] = 1;
+    }
+  }
+
+  // 第二步：查找多数元素
+  for (let key in dict) {
+    if (dict[key] > nums.length / 2) {
+      return Number(key); // key 是字符串，需要转成数字
+    }
+  }
+};
+console.log(majorityElement([3, 2, 3]));
+
+//乘法
+var productExceptSelf = function (nums) {
+  let resfront = [];
+  let resback = []; //倒着再来一个前缀乘
+  let a = 1;
+  let b = 1;
+  for (let i = 0; i < nums.length; i++) {
+    a = a * nums[i];
+    resfront.push(a);
+  }
+  for (let j = nums.length - 1; j > -1; j--) {
+    b = b * nums[j];
+    resback.push(b);
+  }
+  let res = [];
+  for (let m = 0; m < nums.length; m++) {
+    if (m == 0) {
+      res.push(resback[nums.length - m - 2]);
+    } else if (m == nums.length - 1) {
+      res.push(resfront[m - 1]);
+    } else {
+      res.push(resfront[m - 1] * resback[nums.length - m - 2]);
+    }
+  }
+  return res;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+
+//栈实现
+var MinStack = function () {
+  this.val = []; //数组（引用类型），修改它的内容不需要重新赋值， 存储的是数组的【内存地址】
+  this.min = []; //辅助栈，用于维护一个有序数组
+};
+MinStack.prototype.push = function (val) {
+  let stack = this.val; //局部变量stack 也拿到了同一个内存地址的【副本】，修改的话相当于就是修改原数组了
+  let mins = this.min;
+  if (mins.length == 0) {
+    mins.push(val);
+  } else {
+    if (mins[mins.length - 1] >= val) {
+      mins.push(val);
+    } else {
+      let tmp = mins.pop();
+      mins.push(val);
+      mins.push(tmp);
+    }
+  }
+  stack.push(val);
+  return;
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  let stack = this.val;
+  let mins = this.min;
+  let val = stack.pop();
+  const indx = mins.indexOf(val);
+  if (indx != -1) {
+    mins.splice(indx, 1);
+  }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  let stack = this.val;
+  return stack[stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.min[this.min.length - 1];
+};
+
+let st = new MinStack();
+st.push(1);
+st.push(2);
+st.push(3);
+console.log(st.val);
+console.log(st.min);
+
+var maxProduct = function (nums) {
+  let n = nums.length;
+  let dpmax = []; //
+  let dpmin = [];
+  dpmax[0] = nums[0];
+  dpmin[0] = nums[0];
+  for (let i = 1; i < n; i++) {
+    dpmax[i] = Math.max(
+      nums[i],
+      dpmax[i - 1] * nums[i],
+      dpmin[i - 1] * nums[i],
+    );
+    dpmin[i] = Math.min(
+      nums[i],
+      dpmin[i - 1] * nums[i],
+      dpmax[i - 1] * nums[i],
+    );
+  }
+  return Math.max(...dpmax);
+};
+
+console.log(maxProduct([2, 3, 2, -4]));
+
+//排序链表
+var sortList = function (head) {
+  //归并排序
+};
+
+//LRU 缓存模拟
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.cache = [];
+  this.usetime = [];
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  let index = this.cache.indexOf(key);
+  if (key == -1) {
+    return -1;
+  }
+  this.usetime[key] += 1;
+  return key;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  let caches = this.cache;
+  let usetime = this.usetime;
+  let indx = caches.indexOf(value);
+  if (indx == -1) {
+    return -1;
+  }
+  if (caches.length == this.capacity) {
+    let minval = Math.min(...usetime);
+    let minindex = usetime.indexOf(minval);
+    usetime.splice(minindex, 1);
+    caches.splice(minindex, 1);
+  } else {
+    caches.push([key, vlaue]);
+    usetime.push(1);
+  }
+};
+
+//环形链表
+var hasCycle = function (head) {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+//跳跃游戏max
+var maxJumps = function (arr, d) {
+  let n = arr.length;
+  let dp = new Array(n).fill(1); //以i为起始点的最远跳数，初始默认都是1
+  //按 arr [a] 和 arr [b] 的大小比较，排序索引，生成新数组
+  const idx = Array.from({ length: n }, (_, i) => i).sort(
+    (a, b) => arr[a] - arr[b],
+  );
+
+  for (let i of idx) {
+    //向左跳，遇到比他高的就停止跳跃,当然要保证不跳出界，且最多跳跃d
+    for (let j = i - 1; j >= Math.max(i - d, 0); j--) {
+      if (arr[j] >= arr[i]) {
+        break;
+      }
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+
+    //向右跳同理
+    for (let j = i + 1; j <= Math.min(i + d, n - 1); j++) {
+      if (arr[j] >= arr[i]) {
+        break;
+      }
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+  return Math.max(...dp);
+};
+
+//只出现一次的数字
+//位运算
+function singleNumber(nums) {
+  let ans = 0;
+  for (const num of nums) {
+    ans ^= num;
+  }
+  return ans;
+}
+
+//回文子串
+//动态规划？dp[i]表示以 i 结尾的串的回文子串数
+var countSubstrings = function (s) {
+  let n = s.length;
+  let count = 0;
+  function isSubstrings(left, right) {
+    while (s[left] === s[right] && left > -1 && right < n) {
+      count++;
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    isSubstrings(i, i);
+    isSubstrings(i, i + 1);
+  }
+  return count;
+};
+
+//动态规划
+function coinChange(coins, amount) {
+    // 创建 dp 数组
+    const dp = new Array(amount + 1).fill(Infinity);
+
+    // 金额为 0 时不需要硬币
+    dp[0] = 0;
+
+    // 遍历每个金额
+    for (let i = 1; i <= amount; i++) {
+
+        // 尝试每一种硬币
+        for (const coin of coins) {
+
+            // 当前硬币可以使用
+            if (i - coin >= 0) {
+
+                // 状态转移
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    // 无法组成
+    return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+
+var findTargetSumWays = function(nums, target) {
+
+    let sum = 0;
+
+    for (let num of nums) {
+        sum += num;
+    }
+
+    // 不可能组成
+    if ((target + sum) % 2 !== 0) return 0;
+
+    let bagSize = (target + sum) / 2;
+
+    // target 过大
+    if (bagSize < 0) return 0;
+
+    // dp[j] 表示组成 j 的方案数
+    let dp = new Array(bagSize + 1).fill(0);
+
+    // 初始化
+    dp[0] = 1;
+
+    // 遍历物品
+    for (let num of nums) {
+
+        // 0/1 背包倒序
+        for (let j = bagSize; j >= num; j--) {
+
+            dp[j] += dp[j - num];
+        }
+    }
+
+    return dp[bagSize];
+};
+
+
+//set
+var findDisappearedNumbers = function(nums) {
+    let n= nums.length
+    let nums_new = new Set(nums)
+    let res = []
+    for (let i = 1 ;i <= n;i ++ ){
+      if(!nums_new.has(i)){
+        res.push(i)
+      }
+    }
+    return res
 };
